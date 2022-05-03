60f8ae958c25803802a220f4e263b2bc89587467
Ferenc Orgovan <orgovafn>
2022-05-03 17:26:56 +0200

    Created project

---
+++ src/test/java/hu/nye/progkor/belepteto/BeleptetoApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.nye.progkor.belepteto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeleptetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
